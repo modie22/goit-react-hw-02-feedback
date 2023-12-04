cb80a2634e143708ff9665e5a64bc629c47df354
Kostya Semenchuk <75216256+modie22@users.noreply.github.com>
2023-12-04 20:25:27 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,57 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import {Notification} from './Notification/Notification';
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  countTotalFeedback = () => {
+    return this.state.good + this.state.bad + this.state.neutral;
+  };
+  countPositiveFeedbackPercentage = () => {
+    return this.countTotalFeedback() === 0 ? 0 :((this.state.good / this.countTotalFeedback()) * 100).toFixed(2);
+  };
+  handleClick = e => {
+    this.setState(prevState => {
+      return {
+        [e.target.name]: prevState[e.target.name] + 1,
+      };
+    });
+    this.countTotalFeedback();
+    this.countPositiveFeedbackPercentage();
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const options = Object.keys(this.state);
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+    return (
+      <>
+        <Section title='Please leave feedback'>
+        <FeedbackOptions
+          options={options}
+          handleClick={this.handleClick}
+        ></FeedbackOptions>
+        </Section>
+        <Section title="Statistics">
+        {total===0 ? <Notification message='There is no feedback' /> :
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+          positivePercentage={positivePercentage}
+        ></Statistics>
+        }
+        </Section>
+      </>
+    );
+  }
+}
+export default App;
